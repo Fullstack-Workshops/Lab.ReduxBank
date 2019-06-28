7224770803785e7e8e139e048e76b34859151ce2
Michelle Ureña <mcurena24@gmail.com>
2019-06-28 18:39:33 -0400

    rf solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import loggingMiddleware from 'redux-logger'
 
 const balance = document.getElementById("balance");
 const deposit5 = document.getElementById("deposit5");
@@ -6,13 +7,17 @@ const deposit25 = document.getElementById("deposit25");
 const withdraw5 = document.getElementById("withdraw5");
 const withdraw25 = document.getElementById("withdraw25");
 
-deposit5.onclick = () => store.dispatch({ type: "deposit", amount: 5 })
-deposit25.onclick = () => store.dispatch({ type: "deposit", amount: 25 })
-withdraw5.onclick = () => store.dispatch({ type: "withdraw", amount: 5 })
-withdraw25.onclick = () => store.dispatch({ type: "withdraw", amount: 25 })
+//Action Creators
+const depositMoney = amount => ({
+  type: 'deposit',
+  amount
+})
+const withdrawMoney = amount => ({
+  type: 'withdraw',
+  amount
+})
 
-
-const store = createStore((state = { balance: 0 }, action) => {
+const reducer = (state = { balance: 0 }, action) => {
   switch (action.type) {
     case "deposit":
       return { balance: state.balance + action.amount }
@@ -21,10 +26,16 @@ const store = createStore((state = { balance: 0 }, action) => {
     default:
       return state;
   }
-})
+}
 
-const balanceField = document.getElementById("balance");
+const store = createStore(reducer, applyMiddleware(loggingMiddleware))
+
+deposit5.onclick = () => store.dispatch(depositMoney(5))
+deposit25.onclick = () => store.dispatch(depositMoney(25))
+withdraw5.onclick = () => store.dispatch(withdrawMoney(5))
+withdraw25.onclick = () => store.dispatch(withdrawMoney(25))
+
+
 store.subscribe(() => {
-  console.log("The store state changed. Here is the new state:", store.getState());
-  balanceField.innerText = `$ ${store.getState().balance}`
+  balance.innerText = `$ ${store.getState().balance}`
 })
